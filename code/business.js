6abfa53ff4f1412895ebc19edf79b5168cf9d999
h3110w0r1d1 <dafr0608@gmail.com>
2026-07-20 13:12:16 -0700

    Add files via upload

--- code/business.js
+++ code/business.js
@@ -13,6 +13,33 @@ class Business{
         
     }
 }
-class EmployeeManager{constructor(difficulty){this.test=true;}}
+class EmployeeManager{constructor(difficulty){
+if(!data.calcStartEmployees){
+    this.employees=data.startEmployees;
+}else{
+    this.employees=[3/difficulty,0,0,0]
+}
+this.cost=this.calcEmployeeCost();
+this.profit=this.calcEmployeeProfit();
+}
+calcEmployeeCost(){
+    var cost=0;
+    for(let i=0;i<this.employees.length;i++){
+        cost+=(this.employees[i]*data.employeeCost*(i+1));
+    }return cost;
+}
+calcEmployeeProfit(){
+  if(isNaN(this.cost)||this.cost <=0){
+    this.cost=this.calcEmployeeCost();
+  }
+  var profit=0;
+   for(let i=0;i<this.employees.length;i++){
+        profit+=(this.employees[i]*data.employeeRev*(i+1));
+    }
+    profit=(profit-Number(this.cost));
+    return profit;
+}
+
+}
 module.exports.EmployeeManager=EmployeeManager;
 module.exports.Business=Business;
